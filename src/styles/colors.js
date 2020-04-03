6298fe57310cd9294a63856fef8daf7a81f14a3d
fabriciobg <fabricio.gnd@gmail.com>
2020-04-03 19:07:13 -0300

    Estilos

---
+++ src/styles/colors.js
@@ -0,0 +1,23 @@
+export const titleTextColor = {
+    color: '#E7EBE3'
+}
+
+export const subtitleTextColor = {
+    color: '#E0E4DF'
+}
+
+export const headerBackgroundColor = {
+    backgroundColor: '#0A1E2B'
+}
+
+export const bottomTabIconColor = '#0A1E2B'
+
+export const cardBackgroundColor = '#394B69'
+
+export const dialogTitleColor = '#394B69'
+
+export const dialogContentTextColor = '#0A1E2B'
+
+export const buttonConfirmColor = '#006600'
+
+export const buttonCancelColor = '#cc0000'
