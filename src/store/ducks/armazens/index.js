0068808df73b6ab9899019ad7c0129ae13d5c795
fabriciobg <fabricio.gnd@gmail.com>
2020-04-07 16:20:29 -0300

    Criação do duck Armazem

--- src/store/ducks/armazens/index.js
+++ src/store/ducks/armazens/index.js
@@ -1,9 +1,18 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
 
-const INITIAL_STATE = []
+const INITIAL_STATE = {
+    loading: true,
+    all: [],
+    filter: []
+}
 
 export const loadArmazens = createAction('LOAD_ARMAZENS')
+export const loadingArmazens = createAction('LOADING_ARMAZENS')
+export const filterArmazens = createAction('FILTER_ARMAZENS')
 
 export default createReducer(INITIAL_STATE, {
-    [loadArmazens.type]: (state, action) => [ ...action.payload ]
+    [loadArmazens.type]: (state, action) => ({ ...state, all: [...action.payload], filter: [...action.payload] }),
+    [loadingArmazens.type]: (state, action) => ({ ...state, loading: action.payload }),
+    [filterArmazens.type]: (state, action) => ({ ...state, filter: state.all.filter(armazem => armazem.nome.includes(action.payload.toUpperCase())) }),
+
 })
